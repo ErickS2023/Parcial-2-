c990caea112c98d6bfc2747b49cee9a9883c1966
ErickS2023 <141955911+ErickS2023@users.noreply.github.com>
2023-10-03 12:22:05 -0600

    Add files via upload

---
+++ examen 2 parcial.cpp
@@ -0,0 +1,22 @@
+//detectar los  errores en el programa
+#include<stdio.h>
+int main()
+{
+	int n1, n2, resultado, suma;
+	printf("Introducir un numero:\n");
+	scanf("%d", &n1);
+	printf("Introducir otro numero:\n");
+	scanf("%d", n2);
+	printf("Cuanto suman:\n");
+	scanf("%d", &suma);
+	resultado == n1 + n2;
+	if (suma == resultado){
+    printf("Correcto\n");
+	}
+	else{
+		
+		printf("INCORRECTO: La suma es %x\n",resultado);
+	}
+	return 0;
+}
+
---
+++ numero del 1 al 100 con un multiplo de 3.cpp
@@ -0,0 +1,19 @@
+
+//programa que para numero del 1 al 100 si dicho numero es multiplo de 3 
+#include<stdio.h>
+int main()
+{
+	int x,sum=0;
+	for(x=1;x<=100;x++)
+	{
+		if(x%3==0){
+			printf("%d\n",x);
+			
+		}
+	}
+	printf("Numeros multiplos del 3 del 1 al 100 ");
+	return 0;
+}
+ 
+
+
